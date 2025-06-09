6105780d4d66261b6aa2fb6f8819fcd09012a2b5
ilyasa1211 <muhammadilyasafadhlih@gmail.com>
2025-06-09 20:35:42 +0700

    Remove: dist directory

--- client/dist/player.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class Player {
-    //   public id: string;
-    constructor(name, character) {
-        this.name = name;
-        this.character = character;
-        // this.id = Utils.generateUUID();
-    }
-}
