d38743598e6371e72b6c068117373059f5b62b33
ilyasa1211 <muhammadilyasafadhlih@gmail.com>
2025-06-09 20:35:42 +0700

    Remove: dist directory

--- client/dist/display.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class Display {
-    render(...drawables) {
-        drawables.forEach((drawable) => drawable.draw());
-    }
-    refresh(...refreshables) {
-        refreshables.forEach((refreshable) => refreshable.refresh());
-    }
-}
